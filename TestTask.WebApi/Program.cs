072661010b15575cf9bbf9486b1df31eb83c4e2c
Vazovsk1y <anolrey.b@gmail.com>
2024-02-01 19:22:20 +0300

    -configured serilog logging.

--- TestTask.WebApi/Program.cs
+++ TestTask.WebApi/Program.cs
@@ -3,8 +3,14 @@ using TestTask.Application.Extensions;
 using TestTask.WebApi.Extensions;
 using TestTask.WebApi.Endpoints;
 using TestTask.WebApi;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((context, conf) =>
+{
+    conf.ReadFrom.Configuration(context.Configuration);
+});
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -17,6 +23,7 @@ builder.Services
 
 var app = builder.Build();
 
+app.UseSerilogRequestLogging();
 app.UseExceptionHandler();
 
 app.UseSwagger();
