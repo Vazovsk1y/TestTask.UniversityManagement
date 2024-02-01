086fda0f5b28af1b9c1e68114262ef61134ca8fe
Vazovsk1y <anolrey.b@gmail.com>
2024-02-01 18:20:23 +0300

    -added global exceptions handler.

--- TestTask.WebApi/Program.cs
+++ TestTask.WebApi/Program.cs
@@ -2,17 +2,22 @@ using TestTask.DAL.Extensions;
 using TestTask.Application.Extensions;
 using TestTask.WebApi.Extensions;
 using TestTask.WebApi.Endpoints;
+using TestTask.WebApi;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddExceptionHandler<ExceptionsHandler>();
+builder.Services.AddProblemDetails();
 
 builder.Services
     .AddDataAccessLayer(builder.Configuration)
     .AddApplicationLayer();
 
 var app = builder.Build();
+
+app.UseExceptionHandler();
 
 app.UseSwagger();
 app.UseSwaggerUI();
